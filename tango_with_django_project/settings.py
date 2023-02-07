71bba2e4ae9361b15160b47cc761473e8d40ae1b
Hugh O'Neill <2579978o@student.gla.ac.uk>
2023-02-07 10:52:39 +0000

    up to chapter 10 (chapter 9 tests)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,6 +23,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
+
 
 
 # Quick-start development settings - unsuitable for production
